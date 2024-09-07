34ee5b29cdd831ea2f3a1cf796dcc8a5291e905b
MeechaPooch <powchm@catlin.edu>
2024-09-07 01:15:51 -0700

    Relative file path

--- src/DoodleDigits.java
+++ src/DoodleDigits.java
@@ -5,8 +5,9 @@ import graphics.DoodleDigitsWindow;
 
 public class DoodleDigits {
     public static void main(String... args) {
-//        new DoodleDigitsWindow(LinearNetwork.load("data/networks/MNIST/1L-500=95A.mpnn"), new MNISTPixelTransformer());
-        new DoodleDigitsWindow(LinearNetwork.load("data/networks/DIGITS-AK/1L-50-OneHotSt.mpnn"), new AlpaydinKaynakPixelTranfsormer());
+    //    new DoodleDigitsWindow(LinearNetwork.load("data/networks/MNIST/1L-500=95A.mpnn"), new MNISTPixelTransformer());
+        // new DoodleDigitsWindow(LinearNetwork.load("data/networks/DIGITS-AK/training-demo.mpnn"), new AlpaydinKaynakPixelTranfsormer());
+        new DoodleDigitsWindow(LinearNetwork.load("../data/networks/DIGITS-AK/1L-50-OneHotSt.mpnn"), new AlpaydinKaynakPixelTranfsormer());
 //        new DoodleDigitsWindow(LinearNetwork.load("data/networks/DIGITS-AK/2L-32,16.mpnn"), new AlpaydinKaynakPixelTranfsormer());
 //        new DoodleDigitsWindow(LinearNetwork.load("data/networks/DIGITS-AK/OneLayer32Nodes.mpnn"));
     }
